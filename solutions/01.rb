f158f2fafaecfdba4b3ff4c771b50491756cd301
Maria <m_duleva@abv.bg>
2015-01-19 18:59:38 +0200

    add solution to task 1

---
+++ solutions/01.rb
@@ -0,0 +1,18 @@
+def fibonacci_series(number)
+  return 1 if number <= 2
+  fibonacci_series(number - 1) + fibonacci_series(number - 2)
+end
+
+def lucas_series(number)
+  return 2 if number == 1
+  return 1 if number == 2
+  lucas_series(number - 1) + lucas_series(number - 2)
+end
+
+def series(kind_of_series, number)
+  case kind_of_series
+    when "fibonacci" then fibonacci_series(number)
+    when "lucas"     then lucas_series(number)
+    when "summed"    then fibonacci_series(number) + lucas_series(number)
+  end
+end
